abfcdb2242ce08827c3ccd5dc3c0517fbae1384a
Xiong Yuan <78838190+xyuan04@users.noreply.github.com>
2021-05-01 00:05:29 -0400

    updated timer.service

--- bootcamp/src/app/timer.service.ts
+++ bootcamp/src/app/timer.service.ts
@@ -8,49 +8,47 @@ export class TimerService {
   constructor() { }
 
   timeConvertor(time: string): string {
-    let PM = time.match('PM') ? true : false
+    const PM = time.match('PM') ? true : false;
     let hour;
     let min;
-    let numbers = ["zero", "one", "two", "three", "four",
-    "five", "six", "seven", "eight", "nine",
-    "ten", "eleven", "twelve", "thirteen",
-    "fourteen", "fifteen", "sixteen", "seventeen",
-    "eighteen", "nineteen", "twenty", "twenty one",
-    "twenty two", "twenty three", "twenty four",
-    "twenty five", "twenty six", "twenty seven",
-    "twenty eight", "twenty nine", "thirty", "thirty one",
-    "thirty two", "thirty three", "thirty four", "thirty five",
-    "thirty six", "thirty seven", "thirty eight", "thirty nine",
-    "forty", "forty one", "forty two", "forty three", "forty four",
-    "forty five", "forty six", "forty seven", "forty eight", "forty nine",
-    "fifty", "fifty one", "fifty two", "fifty three", "fifty four",
-    "fifty five", "fifty six", "fifty seven", "fifty eight", "fifty nine"];
-    
-    let timeArray = time.split(':')
+    const numbers = ['Zero', 'One', 'Two', 'Three', 'Four',
+    'Five', 'Six', 'Seven', 'Eight', 'Nine',
+    'Ten', 'Eleven', 'Twelve', 'Thirteen',
+    'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen',
+    'Eighteen', 'Nineteen', 'Twenty', 'Twenty One',
+    'Twenty Two', 'Twenty Three', 'Twenty Four',
+    'Twenty Five', 'Twenty Six', 'Twenty Seven',
+    'Twenty Eight', 'Twenty Nine', 'Thirty', 'Thirty One',
+    'Thirty Two', 'Thirty Three', 'Thirty Four', 'Thirty Five',
+    'Thirty Six', 'Thirty Seven', 'Thirty Eight', 'Thirty Nine',
+    'Forty', 'Forty One', 'Forty Two', 'Forty Three', 'Forty Four',
+    'Forty Five', 'Forty Six', 'Forty Seven', 'Forty Eight', 'Forty Nine',
+    'Fifty', 'Fifty One', 'Fifty Two', 'Fifty Three', 'Fifty Four',
+    'Fifty Five', 'Fifty Six', 'Fifty Seven', 'Fifty Eight', 'Fifty Nine'];
 
-    
-    if (PM) {
-        hour = 12 + parseInt(timeArray[0],10);
-        min = timeArray[1].replace('PM', '');
-        min = parseInt(min);
-    } else {
-        hour = timeArray[0];
-        hour = parseInt(hour);
-        min = timeArray[1].replace('AM', '');    
-        min = parseInt(min); 
+    if (time === '12:00AM') {
+      return 'Zero Hundred Hours';
     }
 
-    // console.log(hour + ':' + min)
-    
-    if(hour === 0 && min === 0) {
-      return 'Zero Hundred Hours'
-    } else if (min < 10) {
+    const timeArray = time.split(':');
+
+
+    if (PM) {
+        hour = 12 + parseInt(timeArray[0], 10);
+        min = timeArray[1].replace('PM', '');
+        min = parseInt(min, 10);
+    } else {
+        hour = timeArray[0];
+        hour = parseInt(hour, 10);
+        min = timeArray[1].replace('AM', '');
+        min = parseInt(min, 10);
+    }
+
+    if (min < 10) {
       return `${numbers[hour]} Hundred and Zero ${numbers[min]} Hours`;
     } else {
       return `${numbers[hour]} Hundred and ${numbers[min]} Hours`;
     }
 }
-
-timeConverter('1:30PM')
 
 }
