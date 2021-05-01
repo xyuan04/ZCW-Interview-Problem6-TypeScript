6bbe4192f300346c6b35908663329fc8757e6c78
Xiong Yuan <78838190+xyuan04@users.noreply.github.com>
2021-05-01 01:17:58 -0400

    updated

--- bootcamp/src/app/timer/timer.component.ts
+++ bootcamp/src/app/timer/timer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TimerService} from '../timer.service';
 
 @Component({
   selector: 'app-timer',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.css']
 })
 export class TimerComponent implements OnInit {
+  militaryTime = '';
 
-  constructor() { }
+  constructor(private timerService: TimerService) { }
 
   ngOnInit(): void {
+  }
+
+  convertTime(time: string): void {
+    this.militaryTime = this.timerService.timeConvertor(time);
   }
 
 }
