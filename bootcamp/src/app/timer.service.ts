40db04ebd24397979820732416b8a3cc35b3daec
Xiong Yuan <78838190+xyuan04@users.noreply.github.com>
2021-04-30 23:08:35 -0400

    Update timer.service.ts

--- bootcamp/src/app/timer.service.ts
+++ bootcamp/src/app/timer.service.ts
@@ -10,20 +10,45 @@ export class TimerService {
   timeConvertor(time) {
     let PM = time.match('PM') ? true : false
     let hour;
-    let sec;
+    let min;
+    let numbers = ["zero", "one", "two", "three", "four",
+    "five", "six", "seven", "eight", "nine",
+    "ten", "eleven", "twelve", "thirteen",
+    "fourteen", "fifteen", "sixteen", "seventeen",
+    "eighteen", "nineteen", "twenty", "twenty one",
+    "twenty two", "twenty three", "twenty four",
+    "twenty five", "twenty six", "twenty seven",
+    "twenty eight", "twenty nine", "thirty", "thirty one",
+    "thirty two", "thirty three", "thirty four", "thirty five",
+    "thirty six", "thirty seven", "thirty eight", "thirty nine",
+    "forty", "forty one", "forty two", "forty three", "forty four",
+    "forty five", "forty six", "forty seven", "forty eight", "forty nine",
+    "fifty", "fifty one", "fifty two", "fifty three", "fifty four",
+    "fifty five", "fifty six", "fifty seven", "fifty eight", "fifty nine"];
     
     time = time.split(':')
-    let min = time[1]
+
     
     if (PM) {
-        hour = 12 + parseInt(time[0],10)
-        sec = time[2].replace('PM', '')
+        hour = 12 + parseInt(time[0],10);
+        min = time[1].replace('PM', '');
+        min = parseInt(min);
     } else {
-        hour = time[0]
-        sec = time[2].replace('AM', '')       
+        hour = time[0];
+        hour = parseInt(hour);
+        min = time[1].replace('AM', '');    
+        min = parseInt(min); 
     }
+
+    console.log(hour + ':' + min)
     
-    console.log(hour + ':' + min + ':' + sec)
+    if(hour === 0 && min === 0) {
+      return 'Zero Hundred Hours'
+    } else if (min < 10) {
+      return `${numbers[hour]} Hundred and Zero ${numbers[min]} Hours`;
+    } else {
+      return `${numbers[hour]} Hundred and ${numbers[min]} Hours`;
+    }
 }
 
 }
